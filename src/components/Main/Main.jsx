02b3aa2698c6f5f9cfacb942c02c28de3a690c97
SURBHI_SOMANATHE <surbhisomnathe7777@gmail.com>
2022-08-07 14:01:58 +0530

    for hackathon

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+
+import useStyles from './styles';
+import Form from './Form/Form';
+import List from './Main/List/List';
+
+const Main = () => {
+    const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+        <CardHeader title="Money Manager" subheader="Powered by SurbhiS" />
+        <CardContent>
+        <Typography align="center" variant="h5"> </Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+            { /*Infocard....*/}
+  
+        </Typography>
+      <Divider />
+      <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+      <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Main;
---
+++ src/components/Main/Main/List/List.jsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+import React, { useContext } from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+
+import { MoneyManagerContext } from '../../../../context/context'
+
+import useStyles from './styles';
+// import { useContext } from 'react';
+
+const List = () => { 
+     const classes = useStyles();
+     
+       const { deleteTransaction, transactions } = useContext(MoneyManagerContext);
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map((transaction) => (
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={transaction.category} secondary={`rs${transaction.amount} - ${transaction.date}`} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  );
+}
+
+export default List
